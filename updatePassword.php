4112765c69624376ec80375095c2fc05cde07324
iafshar <iafshar001@gmail.com>
2023-08-23 09:24:59 -0400

    Styled the updating a password form to make it look consistent with the rest of the pages.

--- updatePassword.php
+++ updatePassword.php
@@ -9,7 +9,9 @@ echo $_SESSION["ErrorReset"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Update Password</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div class="signUp">
@@ -17,9 +19,9 @@ echo $_SESSION["ErrorReset"];
 			<?php $_SESSION["ErrorReset"]=NULL;?>
 			method="post"> <!--Makes sure that the same errors are not presented if the user goes to a different page-->
 				<div>
-					<input type="password" name="Password1" placeholder="Password" id="password" autocomplete="new-password" required>
+					<input type="password" name="Password1" placeholder="New Password" id="password" autocomplete="new-password" required>
 					<input type="password" name="Password2" placeholder="Confirm Password" id="confirm-password" autocomplete="new-password" required>
-					<input type="submit" value="Sign-Up">
+					<input type="submit" value="Enter">
 				</div>
 			</form>
 		</div>

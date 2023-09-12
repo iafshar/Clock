5aed1f499b381ca6a38352f50199a6f88f3ba7fe
iafshar <iafshar001@gmail.com>
2023-09-12 13:25:24 -0400

    if a user clicks on a search result or autocomplete suggestion, it gets added to their searches.

--- getOtherUserClocks.php
+++ getOtherUserClocks.php
@@ -8,17 +8,23 @@ require_once __DIR__ . '/dbConnect.php';
 // connecting to db
 $db = new DB_CONNECT();
 
+$MyUserID = $_SESSION["UserID"];
+
 $SearchedUser = $_GET['Username'];
 $IDSearch = "SELECT * FROM `Users` WHERE Username='$SearchedUser'";
-$result1 = $db->get_con()->query($IDSearch);
+$result = $db->get_con()->query($IDSearch);
 
     // looping through all results
 
-while ($row = $result1->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
 
     $SearchedUserID = $row["UserID"];
 
 }
+
+$addSearch = "INSERT INTO `Searches` (UserID, Search)
+  VALUES ('$MyUserID','$SearchedUser')";
+$db->get_con()->query($addSearch);
 
 $_SESSION["SearchedUserID"] = $SearchedUserID;
 $_SESSION["SearchedUsername"] = $SearchedUser;

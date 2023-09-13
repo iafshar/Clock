99054be79493d67deb22b69c07e77cade72a7b9e
iafshar <iafshar001@gmail.com>
2023-09-12 20:24:42 -0400

    removed resetPassword and resetPassword2 from local storage in start.php

--- start.php
+++ start.php
@@ -25,6 +25,8 @@ session_unset();
     		localStorage.removeItem("signUpEmail");
 			localStorage.removeItem("signUpPassword");
 			localStorage.removeItem("signUpPassword2");
+			localStorage.removeItem("resetPassword"); 
+			localStorage.removeItem("resetPassword2"); 
 		</script>
 	</body>
 </html>

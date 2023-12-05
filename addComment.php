568c9da1304e4b75a52a832f2afffe4f64ed0ac3
iafshar <iafshar001@gmail.com>
2023-12-05 15:00:34 -0800

    added ajax for comments on feed, passed in clockID to addComment.php to remove unneccessary code and removed unnecessary hidden input fields and removed form aspect

--- addComment.php
+++ addComment.php
@@ -7,50 +7,14 @@ $db = new DB_CONNECT();
 // Create connection
 $conn = $db->get_con();
 
-if(isset($_POST['location'])){
-  if($_SESSION["Premium"] == 1){
-    if(isset($_POST['Maker'])){
-      
-      $Maker = $_POST['Maker'];
-      $GetUserID = "SELECT * FROM Users Where Username='$Maker'";
-      $resultUser = mysqli_query($conn, $GetUserID);
-      if ($resultUser->num_rows > 0) {
-          while ($row = $resultUser->fetch_assoc()) {
-              $UserID = $row["UserID"];
-          }
-      }
-    }
-    elseif (isset($_POST['MakerID'])) {
-      $UserID = $_POST['MakerID'];
-    }
-    else {
-      $UserID = $_SESSION["SearchedUserID"];
-    }
-    if(isset($_POST['clockName']) && strlen($_POST['comment']) > 0){
-      
+if(isset($_POST['clockID']) && isset($_POST['comment']) && $_SESSION["Premium"] == 1 && strlen($_POST['comment']) > 0){
+  $ClockID = $_POST['clockID'];
+  $Date = date("Y-m-d H:i:s");
+  $Comment = mysqli_real_escape_string($conn, $_POST['comment']);
+  $AddComment = "INSERT INTO Comments (ClockID, Comment, Date)
+    VALUES ('$ClockID','$Comment', '$Date')";
 
-      $clockName = $_POST['clockName'];
-      $GetClockID = "SELECT * FROM Clocks WHERE UserID='$UserID' AND Name='$clockName'";
-      $resultClock = mysqli_query($conn, $GetClockID);
-      if ($resultClock->num_rows > 0) {
-          while ($row = $resultClock->fetch_assoc()) {
-              $ClockID = $row["ClockID"];
-          }
-      }
-
-      $Date = date("Y-m-d H:i:s");
-      $Comment = mysqli_real_escape_string($conn, $_POST['comment']);
-      $AddComment = "INSERT INTO Comments (ClockID, Comment, Date)
-        VALUES ('$ClockID','$Comment', '$Date')";
-
-      mysqli_query($conn, $AddComment);
-    }
-    header("Location:".$_POST['location']);
-  }
-  else{
-    $_SESSION["Error"] = "You need to upgrade to premium to be able to comment.";
-    header("Location:myClocks.php");
-  }
+  mysqli_query($conn, $AddComment);
 }
-
- ?>
+echo 1;
+?>

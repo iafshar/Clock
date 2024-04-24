2fb8e1061d0cabf3469d515e9d04af091b0fcba4
iafshar <iafshar001@gmail.com>
2024-04-24 13:40:48 -0700

    removing space

--- dbConfig.php
+++ dbConfig.php
@@ -1,9 +1,7 @@
 <?php
-
 /*
  * All database connection variables
  */
-
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password
 define('DB_DATABASE', "ClockDB"); // database name

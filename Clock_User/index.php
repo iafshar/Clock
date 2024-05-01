3429423b6d51ea008ad644c59d2b58c11537f332
iafshar <iafshar001@gmail.com>
2024-05-01 12:29:40 -0700

    changed links to stylesheets in user clock to begin with https instead of http

--- Clock_User/index.php
+++ Clock_User/index.php
@@ -7,9 +7,9 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+  <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="https://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
   <link href="http://localhost:8080/Clock/css/postLanding.css" rel="stylesheet" type="text/css">
   <script src="../functions.js"></script>
   <script>

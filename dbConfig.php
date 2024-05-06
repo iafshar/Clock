4f051dae7027d7ead788a59ae77b378c6519d687
iafshar <iafshar001@gmail.com>
2024-05-05 20:02:43 -0700

    added sendEmail and config back

---
+++ dbConfig.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * All database connection variables
+ */
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', ""); // db password
+define('DB_DATABASE', "ClockDB"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
